3da9867fecf8df3bedb0ec8a469cd2d28cdb56ce
qamarmukhtar <qumerm1@1gmail.com>
2021-01-13 12:46:15 +0530

    ui and bugs

---
+++ app/src/main/java/com/techqamar/myapplication/Ordered_List.java
@@ -0,0 +1,14 @@
+package com.techqamar.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ordered_List extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ordered__list);
+    }
+}
